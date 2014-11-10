85c9505abd205484983f4ec1da344ba2c3145d4f
Bram Cordie <bramcordie@gmail.com>
2014-11-10 15:01:54 +0100

    Save job progress while logging

--- app/scripts/services/joblogger.js
+++ app/scripts/services/joblogger.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* jshint sub: true */
+
 /**
  * @ngdoc service
  * @name udbApp.jobLogger
@@ -8,26 +10,53 @@
  * Service in the udbApp.
  */
 angular.module('udbApp')
-  .service('jobLogger', ['udbSocket', function jobLogger(udbSocket) {
-    var log = [];
+  .service('jobLogger', ['udbSocket', function jobLogger(udbSocket, udbApi) {
+    var jobs = {};
 
-    var logEvent = function (event) {
-      log.push(event);
-      console.log(event);
+    function jobStarted (data) {
+      var job = jobs[data['job_id']] = {
+        id: data['job_id'],
+        events: {},
+        state: 'started'
+      };
+
+      console.log('job with id: ' + job.id + ' started');
+    }
+
+    function jobFinished (data) {
+      var job = jobs[data['job_id']];
+
+      job.state = 'finished';
+
+      console.log('job with id: ' + job.id + ' finished');
+    }
+
+    function eventWasTagged (data) {
+      var jobId = data['job_id'],
+          eventId = data['event_id'],
+          job = jobs[jobId],
+          event;
+
+      if(job) {
+        event = job.events[eventId];
+        if(!event) {
+          event = job.events[eventId] = {
+            id: eventId
+          };
+        }
+
+        event.tagged = true;
+      }
+
+      console.log('Tagged event: ' + event.id + '. ' + _.size(job.events) + ' events tagged so far.');
+    }
+
+    udbSocket.on('event_was_tagged', eventWasTagged);
+    udbSocket.on('job_started', jobStarted);
+    udbSocket.on('job_finished', jobFinished);
+
+    this.getJobs = function () {
+      return jobs;
     };
 
-    udbSocket.on('event_was_tagged', function (data) {
-      data.event = 'event was tagged';
-      logEvent(data);
-    });
-
-    udbSocket.on('job_started', function (data) {
-      data.event = 'job started';
-      logEvent(data);
-    });
-
-    udbSocket.on('job_finished', function (data) {
-      data.event = 'job finished';
-      logEvent(data);
-    });
   }]);
