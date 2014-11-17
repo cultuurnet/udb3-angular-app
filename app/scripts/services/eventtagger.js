1cdc0721b8e4f8a52d527a21fe7327ede1b22bcf
Bram Cordie <bramcordie@gmail.com>
2014-11-17 10:46:45 +0100

    Use event tagger service to tag by ID or query

---
+++ app/scripts/services/eventtagger.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name udbApp.EventTagger
+ * @description
+ * # EventTagger
+ * Service in the udbApp.
+ */
+angular.module('udbApp')
+  .service('eventTagger', function EventTagger(jobLogger, UdbApi) {
+
+    /**
+     * @param {string[]} eventIds
+     * @param {string} label
+     */
+    this.tagEventsById = function (eventIds, label) {
+      var jobPromise = UdbApi.tagEvents(eventIds, label);
+
+      jobPromise.success(function (jobData) {
+        var jobId = jobData.commandId;
+        jobLogger.createJob(jobId, _.map(eventIds, function(id) {
+          return { 'id': id };
+        }), label);
+      });
+    };
+
+    /**
+     *
+     * @param {string} query
+     * @param {string} label
+     */
+    this.tagQuery = function (query, label) {
+      var jobPromise = UdbApi.tagQuery(query, label);
+
+      jobPromise.success(function (jobData) {
+        var jobId = jobData.commandId;
+        jobLogger.createJob(jobId, jobData.eventCount, label);
+      });
+
+    };
+  });
---
+++ test/spec/services/eventtagger.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('Service: eventTagger', function () {
+
+  // load the service's module
+  beforeEach(module('udbApp'));
+
+  // instantiate service
+  var EventTagger;
+  beforeEach(inject(function (_EventTagger_) {
+    EventTagger = _EventTagger_;
+  }));
+
+});
