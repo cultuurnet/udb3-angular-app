37e6f02eae112b4a4d5e1a67413f18670ddff592
Bram Cordie <bramcordie@gmail.com>
2014-11-20 09:10:48 +0100

    iii-88: Add translator service

---
+++ test/spec/services/eventtranslator.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('Service: EventTranslator', function () {
+
+  // load the service's module
+  beforeEach(module('udbApp'));
+
+  // instantiate service
+  var EventTranslator;
+  beforeEach(inject(function (_EventTranslator_) {
+    EventTranslator = _EventTranslator_;
+  }));
+
+});
