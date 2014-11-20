c7755cc024dfe9e6e22de876b99a50149c9f25e8
Bram Cordie <bramcordie@gmail.com>
2014-11-20 17:46:43 +0100

    iii-94: Inline editing of event labels

--- app/scripts/services/eventtagger.js
+++ app/scripts/services/eventtagger.js
@@ -10,6 +10,47 @@
 angular.module('udbApp')
   .service('eventTagger', function EventTagger(jobLogger, UdbApi) {
 
+    var eventTagger = this;
+
+    // keep a cache of all the recently used labels
+    eventTagger.recentLabels = ['some', 'recent', 'label'];
+
+    function updateRecentLabels () {
+      var labelPromise = UdbApi.getRecentLabels();
+
+      labelPromise.then(function (labels) {
+        eventTagger.recentLabels = labels;
+      });
+    }
+    // warm up the cache
+    updateRecentLabels();
+
+    /**
+     * Tag an event with a label
+     * @param {string} eventId
+     * @param {string} label
+     */
+    this.tag = function (eventId, label) {
+      var jobPromise = UdbApi.tagEvent(eventId, label);
+
+      jobPromise.success(function (jobData) {
+        jobLogger.createTranslationJob(jobData.commandId, 'tag evenement met label: ' + label);
+      });
+    };
+
+    /**
+     * Untag a label from an event
+     * @param {string} eventId
+     * @param {string} label
+     */
+    this.untag = function (eventId, label) {
+      var jobPromise = UdbApi.untagEvent(eventId, label);
+
+      jobPromise.success(function (jobData) {
+        jobLogger.createTranslationJob(jobData.commandId, label + ' verwijderen van evenement');
+      });
+    };
+
     /**
      * @param {string[]} eventIds
      * @param {string} label
