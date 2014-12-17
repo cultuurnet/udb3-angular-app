8a7452874c6016f9b4a9b486b6e5c77e57f2cbe9
Bram Cordie <bramcordie@gmail.com>
2014-12-17 15:28:54 +0100

    Add daterangepicker directve and spec files

---
+++ app/scripts/directives/udbqueryeditordaterangepicker.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name udbApp.directive:udbQueryEditorDaterangepicker
+ * @description
+ * # udbQueryEditorDaterangepicker
+ */
+angular.module('udbApp')
+  .directive('udbQueryEditorDaterangepicker', function () {
+    return {
+      templateUrl: 'query-editor-daterangepicker.htmlgit st',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+
+      }
+    };
+  });
