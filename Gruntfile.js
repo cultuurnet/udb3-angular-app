893b813a0010aaffec3668f8884ce453a11cd388
Bram Cordie <bramcordie@gmail.com>
2015-03-20 17:35:24 +0100

    III-236: Don't cache bust images to prevent messed up paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -234,7 +234,6 @@ module.exports = function (grunt) {
         src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/css/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
