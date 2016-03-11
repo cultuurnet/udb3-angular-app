c939401030733e71f68906759dfe31c2c3305b79
Nick <nick@2dotstwice.be>
2016-03-11 09:48:48 +0100

    III-820: add google tag manager code

--- app/scripts/controllers/app.js
+++ app/scripts/controllers/app.js
@@ -12,8 +12,23 @@ angular
   .controller('AppCtrl', AppController);
 
 /* @ngInject */
-function AppController($scope) {
+function AppController($scope, appConfig, $window) {
   $scope.showJobLog = false;
+
+  // Load Google Tag Manager only when the key is not empty
+  if(appConfig.gaTagManager) {
+    angular.element(document).ready(function () {
+      (function (w, d, s, l, i) {
+        w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l !== 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })($window, document, 'script', 'tm', appConfig.gaTagManager);
+    });
+  }
 
   function toggleJobLog() {
     $scope.showJobLog = !$scope.showJobLog;
@@ -21,4 +36,4 @@ function AppController($scope) {
 
   $scope.toggleJobLog = toggleJobLog;
 }
-AppController.$inject = ['$scope'];
+AppController.$inject = ['$scope', 'appConfig', '$window'];
