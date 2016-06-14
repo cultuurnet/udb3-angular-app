d371e3e17e77c3c4356dbebea285c236c40a8707
Stijn Swaanen <swaanenstijn@gmail.com>
2016-06-14 11:51:59 +0200

    III-988 create the side bar

---
+++ app/scripts/controllers/side-bar.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udbApp.controller:HeaderController
+ * @description
+ * # HeaderController
+ * udbApp controller
+ */
+angular
+  .module('udbApp')
+  .controller('sideBarController', sideBarController);
+
+/* @ngInject */
+function sideBarController(uitidAuth, $scope, jobLogger) {
+  var controller = this;
+
+  controller.login = uitidAuth.login;
+  controller.logout = uitidAuth.logout;
+  controller.toggleJobLog = jobLogger.toggleJobLog;
+
+  // TODO: create logic for management permission when user service is ready.
+  controller.userHasManagementPermission = true;
+
+  $scope.$watch(function () {
+    return uitidAuth.getUser();
+  }, function (user) {
+    controller.user = user;
+  }, true);
+}
+sideBarController.$inject = ['uitidAuth', '$scope', 'jobLogger'];
