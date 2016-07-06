e84e21abe6b822f5e03e286129d4c2d3bdc80b17
Nick <nick@2dotstwice.be>
2016-07-06 17:22:19 +0200

    III-1105: load search in a component

--- app/scripts/components/search.component.js
+++ app/scripts/components/search.component.js
@@ -4,13 +4,48 @@ angular
   .module('udbApp')
   .component('searchComponent', {
     bindings: { $router: '<' },
-    templateUrl: 'templates/search.html'
+    templateUrl: 'templates/search.html',
+    controller: searchComponentController
   });
 
-/*function searchComponent($controller, $scope) {
-  var controller = this; // jshint ignore:line
-
-  angular.extend(controller, $controller('Search', {
-    $scope: $scope
+function searchComponentController($controller, $scope, udbApi,
+    LuceneQueryBuilder,
+    $window,
+    $location,
+    $uibModal,
+    SearchResultViewer,
+    offerLabeller,
+    offerLocator,
+    searchHelper,
+    $rootScope,
+    eventExporter,
+    $translate) {
+  angular.extend(this, $controller('Search', {
+    $scope: $scope,
+    udbApi: udbApi,
+    LuceneQueryBuilder: LuceneQueryBuilder,
+    $window: $window,
+    $location: $location,
+    $uibModal: $uibModal,
+    SearchResultViewer: SearchResultViewer,
+    offerLabeller: offerLabeller,
+    offerLocator: offerLocator,
+    searchHelper: searchHelper,
+    $rootScope: $rootScope,
+    eventExporter: eventExporter,
+    $translate: $translate
   }));
-}*/
+}
+
+searchComponentController.$inject = ['$controller', '$scope','udbApi',
+    'LuceneQueryBuilder',
+    '$window',
+    '$location',
+    '$uibModal',
+    'SearchResultViewer',
+    'offerLabeller',
+    'offerLocator',
+    'searchHelper',
+    '$rootScope',
+    'eventExporter',
+    '$translate'];
