f8e5cac7d8c98b1cb4cd5840cf90c9f03856889e
Nick <nick@2dotstwice.be>
2016-09-08 13:10:24 +0200

    III-1232: add moderation count

--- app/scripts/controllers/menu-bar.js
+++ app/scripts/controllers/menu-bar.js
@@ -12,7 +12,16 @@ angular
   .controller('menuBarController', menuBarController);
 
 /* @ngInject */
-function menuBarController(uitidAuth, $scope, jobLogger, authorizationService, authorization, appConfig) {
+function menuBarController(
+  uitidAuth,
+  $scope,
+  jobLogger,
+  authorizationService,
+  RolePermission,
+  appConfig,
+  ModerationManager,
+  $q
+) {
   var controller = this; // jshint ignore:line
 
   controller.login = uitidAuth.login;
@@ -36,14 +45,37 @@ function menuBarController(uitidAuth, $scope, jobLogger, authorizationService, a
     uitidAuth.login();
   };
 
-  $scope.$watch(function () {
-    return uitidAuth.getUser();
-  }, function (user) {
-    controller.user = user;
+  function filterModeratorRoles(roles) {
+    // only show roles with moderator permission
+    var myRoles = _.filter(roles, function(role) {
+      var canModerate = _.filter(role.permissions, function(permission) {
+        return permission === RolePermission.AANBOD_MODEREREN;
+      });
+      return canModerate.length > 0 ? true : false;
+    });
 
+    return $q.resolve(myRoles);
+  }
+
+  function getModerationCount(roles) {
+    var query = '';
+
+    _.forEach(roles, function(value) {
+      query += (query?' OR ':'') + value.constraint;
+    });
+    query = '(' + query + ')';
+
+    ModerationManager
+      .find(query, 10, 0)
+      .then(function(searchResult) {
+        controller.moderationCount = searchResult.totalItems;
+      });
+  }
+
+  function checkMenuRights() {
     // look for permissions
     authorizationService
-      .hasPermission(authorization.manageLabels)
+      .hasPermission(RolePermission.LABELS_BEHEREN)
       .then(function (hasPermission) {
         if(hasPermission) {
           controller.canManageLabels = true;
@@ -52,7 +84,7 @@ function menuBarController(uitidAuth, $scope, jobLogger, authorizationService, a
       });
 
     authorizationService
-      .hasPermission(authorization.manageUsers)
+      .hasPermission(RolePermission.GEBRUIKERS_BEHEREN)
       .then(function (hasPermission) {
         if(hasPermission) {
           controller.canManageUsers = true;
@@ -61,14 +93,37 @@ function menuBarController(uitidAuth, $scope, jobLogger, authorizationService, a
       });
 
     authorizationService
-      .hasPermission(authorization.manageOrganisations)
+      .hasPermission(RolePermission.ORGANISATIES_BEHEREN)
       .then(function (hasPermission) {
         if(hasPermission) {
           controller.canManageOrganizers = true;
           controller.userHasManagementPermission = true;
         }
       });
+  };
+
+  $scope.$watch(function () {
+    return uitidAuth.getUser();
+  }, function (user) {
+    controller.user = user;
+
+    checkMenuRights();
+
+    // get the number of items to moderate
+    ModerationManager
+      .getMyRoles()
+      .then(filterModeratorRoles)
+      .then(getModerationCount);
 
   }, true);
 }
-menuBarController.$inject = ['uitidAuth', '$scope', 'jobLogger', 'authorizationService', 'authorization', 'appConfig'];
+menuBarController.$inject = [
+  'uitidAuth',
+  '$scope',
+  'jobLogger',
+  'authorizationService',
+  'RolePermission',
+  'appConfig',
+  'ModerationManager',
+  '$q'
+];
