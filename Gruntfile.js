2be67a6e151dcd3e4affebef339be71880b146b8
Stijn Swaanen <swaanenstijn@gmail.com>
2018-05-15 09:38:58 +0200

    take care of the enable/disable file in grunt for the building proces

--- Gruntfile.js
+++ Gruntfile.js
@@ -364,7 +364,7 @@ module.exports = function (grunt) {
           cwd: '<%= yeoman.app %>',
           dest: '<%= yeoman.dist %>',
           src: [
-            '*.{ico,png,txt}',
+            '*.{ico,png,txt,disable,enable}',
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
