1f112e7a0b85ad09e10c1a35a569007024724db8
Bert Ramakers <bertramakers@me.com>
2020-07-27 11:41:39 +0200

    III-3347 Update or remove dependencies that depend on graceful-fs@2 and @3
    For node 10+ support, graceful-fs has to be updated to 4+. So all our dependencies using graceful-fs have to be updated to a version that uses @4.
    
    grunt-google-cdn is also affected but has not been updated in 6 years. See https://github.com/btford/grunt-google-cdn/issues/71
    Since it's not a must-have, I've removed it.

--- Gruntfile.js
+++ Gruntfile.js
@@ -348,13 +348,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
-
     // Copies remaining files to places other tasks can use
     copy: {
       dist: {
@@ -529,7 +522,6 @@ module.exports = function (grunt) {
     'concat',
     'ngAnnotate:src',
     'copy:dist',
-    'cdnify',
     'cssmin',
     'uglify',
     'filerev',
